13862379182389638b228cdf3ab4bbedfd726279
CiftciCagatay <ciftcicagatay@outlook.com>
2020-10-03 13:13:24 +0300

    Update message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send("Hello world")
+  res.send("Hello world Burak!")
 })
 
 app.listen(PORT, () => console.log("Application listening on port " + PORT))
